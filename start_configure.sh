1cb2fa155e0381957e1993e0e0ddd8fcf804fa56
pr0g88 <98094639+pr0g88@users.noreply.github.com>
2022-10-20 21:03:21 +0300

    Add files via upload

---
+++ start_configure.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Resolv
+
+echo "Configure resolv.conf ..."
+echo -n "Enter nameserver: "
+read ns
+
+echo -n "Enter the full domain name: "
+read domain
+
+echo "nameserver $ns" > test.txt
+echo "search $domain" >> test.txt
+
+# Network adapter
+
+echo "Configure network ..."
+echo -n "Enter ip address: "
+read addr
+
+echo -n "Enter netmask: "
+read nmask
+
+echo -n "Enter gateway: "
+read gate
+
+int=`ls /sys/class/net/ | xargs | cut -d ' ' -f 1` 
+
+echo "
+source /etc/network/interfaces.d/*
+
+auto lo
+iface lo inet loopback
+
+auto $int
+iface $int inet static
+        address $addr
+        netmask $nmask
+        gateway $gate
+
+" > net.txt
+
+systemctl restart networking.service
+
+echo "*** CONFIGURATION CHECK ... ###"
+cat /etc/resolv.conf
+ip a | grep $int
+ping -c 4 $gate
